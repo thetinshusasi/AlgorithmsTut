dba89e792fd269381229592086adbf87392d2376
Tinshu Sasi <thetinshusasi@gmail.com>
2019-06-09 21:11:29 +0530

    Binary Search

--- AlgorithmsTut/AlgorithmsTut/Program.cs
+++ AlgorithmsTut/AlgorithmsTut/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AlgorithmsTut.SearchingAlgos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,35 @@ namespace AlgorithmsTut
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Binary Search");
+            Console.WriteLine("search for 7 number");
+            CallBinaryRecursiveSearch(7);
+            Console.WriteLine("search for 10 number");
+            CallBinaryRecursiveSearch(10);
+            Console.WriteLine($"/////////////////////////////////////////////////////////");
+            Console.ReadKey();
+
+        }
+
+        public static void CallBinaryRecursiveSearch(int val)
+        {
+            Console.WriteLine("CallBinaryRecursiveSearch");
+            int[] inputArray = new int[]
+            {
+                1,2,3,4,5,7,8,9
+            };
+            var binarySearch = new BinarySearch();
+            var mid = inputArray.Length / 2;
+            var index = binarySearch.RecursiveBinarySearch(inputArray, 0,  inputArray.Length - 1, val);
+            if (index > -1)
+            {
+                Console.WriteLine("value is at position : " + (index + 1));
+            }
+            else
+            {
+                Console.WriteLine("element not in the array");
+            }
+
         }
     }
 }
---
+++ AlgorithmsTut/AlgorithmsTut/SearchingAlgos/BinarySearch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsTut.SearchingAlgos
+{
+    public class BinarySearch
+    {
+
+        public int RecursiveBinarySearch(int[] arr, int startIndex, int endIndex, int val)
+        {
+
+            if (endIndex >= startIndex)
+            {
+                int mid = (startIndex + endIndex) / 2;
+                // If the element is present at the 
+                // middle itself 
+                if (arr[mid] == val)
+                {
+                    return mid;
+                }
+
+                // If element is smaller than mid, then 
+                // it can only be present in left subarray
+                if (arr[mid] > val)
+                {
+                    return RecursiveBinarySearch(arr, startIndex, mid - 1, val);
+
+                }
+
+                // Else the element can only be present 
+                // in right subarray
+                return RecursiveBinarySearch(arr, mid + 1, endIndex, val);
+
+            }
+
+            // We reach here when element is not present 
+            // in array 
+            return -1;
+        }
+
+
+    }
+
+}
